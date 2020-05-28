f33a62d09bbed2d9bccd959ca9471fe76a42dd4a
Anurag Kumar <anurag@leewayhertz.com>
2020-05-28 14:43:27 +0530

    quick fixes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
+// import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import AppRoutes from './routes';
 import * as serviceWorker from './serviceWorker';
+import store from './reducers'
 
 ReactDOM.render(
     <BrowserRouter>
-        <Provider store={store}>
             <AppRoutes component={AppRoutes}/>
-        </Provider>
     </BrowserRouter>, document.getElementById('root')
 );
 
